21485973410c66e86e31b08c49821f400f67b800
Mazuru Gabriel <gabrielmazuru6@gmail.com>
2026-01-17 18:23:33 +0200

    final states

--- init_ready.ino
+++ init_ready.ino
@@ -1,4 +1,8 @@
 int touch_sensor = 2;
+int button = 3;
+int RED = 4;
+int BLUE = 5;
+int GREEN = 6;
 
 enum State {
   INIT,
@@ -6,29 +10,97 @@ enum State {
   RUN,
   FAULT
 };
+
 State currentState = INIT;
-bool initPrinted = false;
-bool readyPrinted = false;
+State previousState = FAULT;
+
+unsigned long runStartTime = 0;
+unsigned long lastBlinkTime = 0;
+bool blinkState = false;
+
+void setColor(bool r, bool g, bool b) {
+  digitalWrite(RED, r);
+  digitalWrite(GREEN, g);
+  digitalWrite(BLUE, b);
+}
+
 void setup() {
   pinMode(touch_sensor, INPUT);
+  pinMode(button, INPUT_PULLUP);
+
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+
   Serial.begin(9600);
 }
+
 void loop() {
-  int val = digitalRead(touch_sensor);
-  if (currentState == INIT) {
-    if (!initPrinted) {
-      Serial.println("INIT STATE");
-      initPrinted = true;
+  int touchVal = digitalRead(touch_sensor);
+  bool buttonPressed = (digitalRead(button) == LOW);
+
+  if (currentState != previousState) {
+    switch (currentState) {
+      case INIT:
+        Serial.println("INIT STATE");
+        setColor(0, 0, 1);   // albastru
+        break;
+
+      case READY:
+        Serial.println("READY STATE");
+        setColor(0, 1, 0);   // verde
+        break;
+
+      case RUN:
+        Serial.println("RUN STATE");
+        setColor(1, 0, 0);   // rosu
+        runStartTime = millis();
+        break;
+
+      case FAULT:
+        Serial.println("FAULT STATE");
+        break;
     }
-    currentState = READY;
+    previousState = currentState;
   }
-  if (currentState == READY) {
-    if (!readyPrinted) {
-      Serial.println("READY STATE");
-      readyPrinted = true;
-    }
-    if (val == HIGH) {
-      currentState = RUN;
-    }
+
+  /* ================= FSM LOGIC ================= */
+
+  switch (currentState) {
+
+    case INIT:
+      currentState = READY;
+      break;
+
+    case READY:
+      if (touchVal == HIGH && buttonPressed) {
+        currentState = RUN;
+      }
+      break;
+
+    case RUN:
+      if (touchVal == LOW) {
+        currentState = READY;
+      }
+
+      // RUN prea mult timp → FAULT
+      if (millis() - runStartTime > 5000) {
+        currentState = FAULT;
+      }
+      break;
+
+    case FAULT:
+      // LED rosu intermitent
+      if (millis() - lastBlinkTime > 300) {
+        blinkState = !blinkState;
+        setColor(blinkState, 0, 0);
+        lastBlinkTime = millis();
+      }
+
+      // reset din FAULT
+      if (buttonPressed) {
+        currentState = INIT;
+      }
+      break;
   }
 }
