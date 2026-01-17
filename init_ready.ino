901063af59d499a82df97feec18175df05839610
Mazuru Gabriel <gabrielmazuru6@gmail.com>
2026-01-17 16:50:32 +0200

    added all states

--- init_ready.ino
+++ init_ready.ino
@@ -1,5 +1,14 @@
 int touch_sensor = 8;
 
+enum State {
+  INIT,
+  READY,
+  RUN,
+  FAULT
+};
+
+State currentState = INIT;
+
 void setup() {
   pinMode(touch_sensor, INPUT);
   Serial.begin(9600);
@@ -7,11 +16,27 @@ void setup() {
 
 void loop() {
   bool val = digitalRead(touch_sensor);
-  if(val == HIGH){
-    Serial.println(1);
-    delay(100);
-  } else{
-    Serial.println(0);
-    delay(100);
+  switch (currentState) {
+    case INIT:
+      Serial.println("INIT STATE");
+      if(touch_sensor == HIGH){
+        currentState = READY;
+      }else{
+        currentState = FAULT;
+      }
+      delay(500);
+      break;
+    case READY:
+      Serial.println("READY STATE");
+      delay(500)
+      break;
+    case RUN:
+      Serial.println("RUN STATE");
+      delay(500)
+      break;
+    case FAULT:
+      Serial.println("FAULT STATE");
+      delay(500)
+      break;
   }
 }
